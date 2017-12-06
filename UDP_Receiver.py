9e4e4e0d78c007689d876e40a86731d77d11c023
MohamedAOsman <32461259+MohamedAOsman@users.noreply.github.com>
2017-12-06 01:36:46 -0500

    Used to Test UDP Receive Without App

--- UDP_Receiver.py
+++ UDP_Receiver.py
@@ -1,13 +1,13 @@
 import json
 import socket
 
-port = 5001
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = 5002 #port to receive information
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #open socket
 
-sock.bind(("",port))
+sock.bind(("",port)) #can receive from anyone
 
 while True:
-    received = sock.recvfrom(1024)
-    json_obj = json.loads(received[0].decode('utf-8'))
-    print(json_obj)
+    received = sock.recvfrom(1024) #receive information
+    json_obj = json.loads(received[0].decode('utf-8')) #decode it and retreive the json object
+    print(json_obj) #print the json object 
     
