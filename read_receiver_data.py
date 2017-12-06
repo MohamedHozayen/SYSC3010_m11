510a0921e630d97e1a62e05e5a083e1a698a4f85
MohamedAOsman <32461259+MohamedAOsman@users.noreply.github.com>
2017-12-06 01:34:58 -0500

    Send Received Metrics to App
    Metrics are Read from the RF Receiver and are sent to the app using udp

--- read_receiver_data.py
+++ read_receiver_data.py
@@ -1,71 +1,61 @@
-import mysql.connector
 import json
 import socket
-UDP_IP = 'localhost'
-PORT = 5001
+from time import sleep
+UDP_IP = '172.20.10.6'  #ip of device that is receiving                                                                                         
+PORT = 5002 #port the information is being sent to
 def read_receiver():
-     receiver_file=open("receiverFile.txt","r") 
-     receiver_string = receiver_file.read()  #Receive metrics from rf receiver
-     metrics = receiver_string.split(",")   #split by comma delimiter
-     return metrics #return array
+     receiver_file=open("/home/pi/433Utils/RPi_utils/BirdALert.txt","r") #open the file containing the info from rf receiver
+     receiver_string = receiver_file.read() #read file
+     receiver_file.close() #close it
+     metrics = receiver_string.split(",") #separate data using delimiter, and put into array
+     return metrics #return array containing the data
      
 
-def get_Temp(metricsList): 
-     temp = metricsList[0]
-     float_temp = float(temp)
-     return float_temp
+def get_Temp(metricsList): #this function receives the metrics array as a parameter
+     temp = metricsList[0] #first index will always be temp
+     return temp #return the temp
      
-def get_pumpSpeed(metricsList):
-     pump = metricsList[1]
-     float_pump = float(pump)
-     return float_pump
+def get_FlowSpeed(metricsList): #this function receives the metrics array as a parameter
+     flowSpeed = metricsList[1] #second index will always be the flowspeed of the water
+     return flowSpeed #return the flowspeed
+
+def get_FlowML(metricsList):#this function receives the metrics array as a parameter
+     flowML = metricsList[2] #third index will always be the flowspeed in units ml/sec
+     return flowML #return the flowspeed
     
-def get_birdSensed(metricsList):
-     birdSensed = metricsList[2]
-     int_sensed = int(birdSensed)
-     return int_sensed
+def get_birdSensed(metricsList):#this function receives the metrics array as a parameter
+     birdSensed = metricsList[3] #fourth index will always be whether a bird has been sensed or not
+     return birdSensed #return whether the bird was sensed
      
-def get_date(metricsList):
-     date = metricsList[3]
-     return date
+def get_date(metricsList): #this function receives the metrics array as a parameter
+     date = metricsList[4] #fifth index is always the date
+     return date #return date of when metrics were recorded
 
-def insert_into_db(temp, pumpSpeed, Sensed, date):
-    stringDate = "'" + date + "'"
-    cnx = mysql.connector.connect(user='root', password='mohamed01',host='localhost',database = 'TestReceiver') #connect to the database
-    curs = cnx.cursor() #assign a cursor
-    try:
-        curs.execute("INSERT INTO metrics VALUES (%f,%f,%i,%s)" % (temp,pumpSpeed,Sensed,stringDate)) #insert passed values into the database
-        cnx.commit() #commit changes
-        curs.close() #close cursor
-        cnx.close()  #close connection
 
-    except:
-        cnx.rollback() #rollback if any errors happen with the insert
-        curs.close()
-        cnx.close()
-
-def create_json(temp,pumpSpeed,birdSense,date): 
-    pythonDictionary = {'temp':str(temp), 'pumpSpeed': str(pumpSpeed),'birdSensed':str(birdSense), 'date':date} #create a dictionary using the passed paramaters
-    dictionaryToJson = json.dumps(pythonDictionary) #make the dictionary into a json object
-    return dictionaryToJson #return json object
+#function used to create a JSON object to store the metrics 
+def create_json(temp, flowSpeed, flowML, Sensed): #receives the metric values as parameters
+    pythonDictionary = {'temp':str(temp), 'flowSpeed': str(flowSpeed), 'flowML':str(flowML),'birdSensed':str(Sensed)} #creates a python dictionary from the metrics
+    dictionaryToJson = json.dumps(pythonDictionary) #dumps the dictionary into a json object
+    return dictionaryToJson #returns json object
 def main():
     while True:
-        metrics = read_receiver()
-        temp= get_Temp(metrics)
-        pumpSpeed = get_pumpSpeed(metrics)
+        metrics = read_receiver()  #get metrics 
+        if(len(metrics)!=5):  #metrics must be an array with length 5. This line is used to ignore incomplete information from the rf receiver
+             continue
+        temp= get_Temp(metrics) 
+        flowSpeed = get_FlowSpeed(metrics)
+        flowML = get_FlowML(metrics)
         birdSense = get_birdSensed(metrics)
         date = get_date(metrics)
 
-        metricsJson = create_json(temp,pumpSpeed,birdSense,date)
-
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #initialize socket used from data transfer
-        sock.sendto(bytes(metricsJson, 'UTF-8'),(UDP_IP,PORT)) #encode into bytes and send to specified address and port
-        #insert_into_db(temp,pumpSpeed,birdSense,date)
-                    
-        #print ('The temp is %.2f' %temp)
-        #print ('The pumpSpeed is %.2f' %pumpSpeed)
-        #print ('The birdSensed value is %i' %birdSense)
-        #print ('The date is %s' %date)
-        #break
+        metricsJson = create_json(temp,flowSpeed,flowML,birdSense)  #json object is created
+          
+        if(int(birdSense) >1 or int(birdSense)< 0):  #this line is used to ignore garbage values received by the rf receiver
+             continue
+          
+        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #socket is opened
+        sock.sendto(bytes(metricsJson, 'UTF-8'),(UDP_IP,PORT)) #information is encoded into bytes and is sent to the ip at provided port
+        
+          
     
 if __name__ == "__main__": main()   
